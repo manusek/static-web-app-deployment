b397f8e2aa97e7d4a9eef3869eca0ac120bed581
kacper <kacper.dlugosz125@gmail.com>
2025-11-04 09:46:21 +0100

    feat(backend): implement s3 remote state and dynamodb state lock

--- main.tf
+++ main.tf
@@ -1,22 +1,29 @@
+provider "aws" {
+
+  region = "eu-central-1"
+}
+
 terraform {
+  required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket = "amzn-s3-bucket-tfstate"
+    key    = "static-website/terraform.tfstate"
+    region = "eu-central-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt = true
+  }
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      version = "6.19.0"
+      source  = "hashicorp/aws"
+      version = "~> 6.19.0" 
     }
   }
 }
 
-provider "aws" {
-  # Configuration options
-  region = "eu-central-1"
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "my-bucket-5652346"
-
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-  }
+module "s3-website-bucket" {
+  source      = "./modules/s3-website-bucket"
+  bucket_name = var.bucket_name
+  tags        = var.tags
 }
