7e29bee0eba18267254e744502961f17253fc748
kacper <kacper.dlugosz125@gmail.com>
2025-11-05 16:23:19 +0100

    feat(s3-website-bucket): add new variables

--- modules/s3-website-bucket/02_variables.tf
+++ modules/s3-website-bucket/02_variables.tf
@@ -6,24 +6,4 @@ variable "bucket_name" {
 variable "tags" {
   description = "A map of tags to apply to all resources in this module."
   type        = map(string)
-  default     = {} 
 }
-
-variable "index_document" {
-  description = "The name of the index document for static hosting."
-  type        = string
-  default     = "index.html"
-}
-
-variable "error_document" {
-  description = "The name of the error document for 4xx errors."
-  type        = string
-  default     = "error.html"
-}
-
-variable "website_content_path" {
-  description = "The local file path containing website content to upload to S3."
-  type        = string
-  default     = "./website-content" 
-}
-
