f88dd653c141427692e124213a8bb50f107376a0
kacper <kacper.dlugosz125@gmail.com>
2025-11-05 18:30:10 +0100

    feat(waf-security): add waf configuration and integrate with cloudfront

---
+++ modules/waf-security/01_main.tf
@@ -0,0 +1,47 @@
+provider "aws" {
+  alias  = "cloudfront"
+  region = "us-east-1"
+}
+
+###### WAF CONFIGURATION
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_wafv2_web_acl" "waf" {
+  provider    = aws.cloudfront
+  name        = local.waf_name
+  description = "WAF for ${var.project_name}"
+  scope       = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${var.project_name}-waf"
+    sampled_requests_enabled   = true
+  }
+
+  rule {
+    name     = "AWSManagedRulesCommonRuleSet"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "${var.project_name}-common"
+      sampled_requests_enabled   = true
+    }
+  }
+}
---
+++ modules/waf-security/02_variables.tf
@@ -0,0 +1,10 @@
+variable "project_name" {
+  type        = string
+  description = "The name of the project."
+}
+
+variable "environment" {
+  type        = string
+  description = "The deployment environment (e.g., dev, staging, prod)."
+}
+
