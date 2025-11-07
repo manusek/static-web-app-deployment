6501788b50afbe703c6ac34271cb608763b7eb5f
kacper <kacper.dlugosz125@gmail.com>
2025-11-07 09:08:35 +0100

    rename: change file names

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "project_name" {
-  description = "Name of project"
-  type        = string
-}
-
-variable "environment" {
-  description = "Enviroment (dev, staging, prod)"
-  type        = string
-}
