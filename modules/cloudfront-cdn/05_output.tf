c6e30d9812ea6f0c8adbbe337c1b3df7cceaaf14
kacper <kacper.dlugosz125@gmail.com>
2025-11-07 09:06:15 +0100

    feat(cloudfront): add distribution_id to outputs

--- modules/cloudfront-cdn/05_output.tf
+++ modules/cloudfront-cdn/05_output.tf
@@ -2,3 +2,8 @@ output "distribution_arn" {
   description = "The ARN of the CloudFront distribution."
   value       = aws_cloudfront_distribution.cdn.arn
 }
+
+output "distribution_id" {
+  description = "The ID of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.cdn.id
+}
