d8fee4af58256bc03dd6b2b128a199e9353f9e5a
kacper <kacper.dlugosz125@gmail.com>
2025-11-12 11:45:02 +0100

    fix(format): spacing and indentation in Terraform configuration files

--- 02_variables.tf
+++ 02_variables.tf
@@ -1,11 +1,11 @@
 variable "project_name" {
   description = "Name of project"
   type        = string
-  default = "static-website"
+  default     = "static-website"
 }
 
 variable "environment" {
   description = "Enviroment (dev, staging, prod)"
   type        = string
-  default = "dev"
+  default     = "dev"
 }
